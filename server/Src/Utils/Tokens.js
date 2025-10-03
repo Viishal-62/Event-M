e55073efae57e9a22af561fcbb7da757afefebfc
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-10-03 19:38:09 +0530

    cookies updated

--- server/Src/Utils/Tokens.js
+++ server/Src/Utils/Tokens.js
@@ -12,8 +12,8 @@ export const createToken = (id, res) => {
  
    res.cookie("token", token, {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite : "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
